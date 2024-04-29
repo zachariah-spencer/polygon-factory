a68794910fa0d12162868da673ebf62931aa6c21
Zachariah <spencerzachariah@gmail.com>
2024-04-28 23:24:06 -0500

    finish implementation for booster structure

--- scripts/structures/booster.gd
+++ scripts/structures/booster.gd
@@ -4,16 +4,30 @@ extends Structure
 @onready var cockpit_empty_pulse := $CockpitEmptyPulse
 @onready var cockpit_piloted_pulse := $CockpitPilotedPulse
 @onready var boost_orb := $BoostOrb
+@onready var boost_area := $BoostArea
+@onready var boost_cooldown := $BoostCooldown
+@onready var streak_label := $StreakInfo/StreakLabel
+@onready var polygon_combo_label := $StreakInfo/PolygonComboLabel
+@onready var anims := $Anims
+@onready var success_visual := $Visual/SuccessVisual
+@onready var streak_high_score_label := $StreakInfo/StreakHighScoreLabel
+@onready var orb_vfx_trail := $BoostOrb/VFX/Trail
 
 var piloted := false
 var cockpit_exit_offset := Vector2(0, 32)
+var streak := 0
+var streak_high_score := 0
+var boost_on_cooldown := true
 
-var distance := 0.0
-var radius := 106.0
-var speed := 0.75
+var base_boost_amount := 1.0
+var unpiloted_base_speed := 0.75
+var piloted_base_speed := 1.5
+var speed := unpiloted_base_speed
+var target_speed := unpiloted_base_speed
 
 func _ready() -> void:
-	pass
+	success_visual.modulate = Color('ffffff7c')
+	orb_vfx_trail.emitting = true
 
 func _on_cockpit_area_body_entered(player : Node2D) -> void:
 	if not player.exiting_cockpit.is_connected(eject_player):
@@ -24,6 +38,7 @@ func _on_cockpit_area_body_entered(player : Node2D) -> void:
 	
 	piloted = true
 	_toggle_cockpit_vfx()
+	target_speed = piloted_base_speed
 
 func eject_player(player : Node2D) -> void:
 	player.global_position = cockpit_area.global_position + cockpit_exit_offset
@@ -31,6 +46,9 @@ func eject_player(player : Node2D) -> void:
 	
 	piloted = false
 	_toggle_cockpit_vfx()
+	target_speed = unpiloted_base_speed
+	for id in Structures.virtual_structures:
+				Structures.virtual_structures[id].polygons_boost = 1
 
 func _toggle_cockpit_vfx():
 	cockpit_empty_pulse.restart()
@@ -41,8 +59,78 @@ func _toggle_cockpit_vfx():
 	cockpit_piloted_pulse.emitting = piloted
 	cockpit_piloted_pulse.visible = piloted
 
-func _process(delta: float) -> void:
-	distance -= delta
-	var orbit_position := Vector2(sin(distance * speed) * radius, cos(distance * speed) * radius)
+func _physics_process(delta: float) -> void:
+	if piloted:
+		_handle_pilot_input()
 	
-	boost_orb.global_position = orbit_position
+	speed = lerp(speed, target_speed, 0.008)
+	boost_orb.rotation += delta * speed
+
+func _is_boost_successful():
+	return boost_area.get_overlapping_bodies().size() != 0
+
+func _handle_pilot_input():
+	if Input.is_action_just_pressed('boost'):
+		if _is_boost_successful():
+			_boost()
+		else:
+			_end_streak()
+
+func _boost():
+	var new_speed := target_speed + base_boost_amount + float(streak / 4.0)
+	speed += new_speed
+	
+	speed = clamp(speed, 0.0, 11.0)
+	
+	_increment_streak()
+	anims.play('successful_boost')
+	
+	boost_on_cooldown = true
+	boost_cooldown.start()
+
+func _increment_streak():
+	streak += 1
+	streak_label.text = 'Streak\n' + str(streak)
+	
+	if streak > streak_high_score:
+		streak_high_score = streak
+		streak_high_score_label.text = 'Best\n' + str(streak_high_score)
+	
+	match(streak):
+		5:
+			for id in Structures.virtual_structures:
+				Structures.virtual_structures[id].polygons_boost = 2
+				
+			var tween = create_tween()
+			tween.tween_property(polygon_combo_label, 'modulate', Color.WHITE, 0.2)
+			polygon_combo_label.text = '2x Polygons!'
+		10:
+			for id in Structures.virtual_structures:
+				Structures.virtual_structures[id].polygons_boost = 3
+			polygon_combo_label.text = '3x Polygons!'
+		15:
+			for id in Structures.virtual_structures:
+				Structures.virtual_structures[id].polygons_boost = 4
+			polygon_combo_label.text = '4x Polygons!'
+
+func _end_streak():
+	streak = 0
+	streak_label.text = 'Streak\n' + str(streak)
+	
+	var tween = create_tween()
+	tween.tween_property(polygon_combo_label, 'modulate', Color.TRANSPARENT, 0.2)
+	
+	for id in Structures.virtual_structures:
+		Structures.virtual_structures[id].polygons_boost = 1
+	
+	
+	speed = clamp(speed * 0.25, piloted_base_speed, 100000.0)
+
+
+func _on_boost_area_body_exited(_body: Node2D) -> void:
+	if not boost_on_cooldown and streak != 0:
+		_end_streak()
+
+
+func _on_boost_cooldown_timeout() -> void:
+	boost_on_cooldown = false
--- scripts/structures/virtual_structure.gd
+++ scripts/structures/virtual_structure.gd
@@ -8,6 +8,8 @@ var upgrade_1_active := false
 var upgrade_2_active := false
 var upgrade_3_active := false
 
+var polygons_boost := 1
+
 signal polygons_generated(polygons :  int)
 
 var cooldown : float = max_cooldown
@@ -22,4 +24,4 @@ func reduce_cooldown_by(amount : float):
 	cooldown -= amount
 	while cooldown <= 0.0:
 		cooldown = max_cooldown
-		polygons_generated.emit(polygons_increment)
+		polygons_generated.emit(polygons_increment * polygons_boost)
