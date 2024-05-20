f8f303fee660d0aa167f1d07376f0e948e0c06aa
Zachariah <spencerzachariah@gmail.com>
2024-05-20 15:00:11 -0500

    begin sound work

---
+++ scripts/audio_fade_in.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var audio_clip := get_parent()
+@export var duration := 1.0
+
+func _ready():
+	audio_clip.volume_db = -80.0
+	
+	var tween = create_tween()
+	tween.tween_property(audio_clip, 'volume_db', 0.0, duration) 
